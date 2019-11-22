ba231ca1960546025a5a7e30c0420a8c3bd0b8b1
David Tran <david.tran@jayway.com>
2019-11-22 12:59:33 +0100

    initial commit

---
+++ src/main/java/com/datra/ssdata/SsDataApplication.java
@@ -0,0 +1,13 @@
+package com.datra.ssdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datra/ssdata/SsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datra.ssdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
